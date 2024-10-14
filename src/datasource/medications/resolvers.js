ffb13acd50c17aef0760eabd360c61123aa8e3bb
Zeyd Sakız <116222599+ZeydSakiz@users.noreply.github.com>
2024-10-14 13:30:13 +0300

    user ve medications alanları baglandı

--- src/datasource/medications/resolvers.js
+++ src/datasource/medications/resolvers.js
@@ -3,14 +3,15 @@ const medicationTypeDefs = require('./schema')
 
 
 const medications=[
-    {id:'1', medTitle:'arveles',content:'for the headhack',pill:true},
-    {id:'2',medTitle:'aferin',content:'for the stomache',pill:false},
-    {id:'3',medTitle:'Ketorolak',content:'for the tooth',pill:false},
-    {id:'4',medTitle:'Sulindak',content:'for the back',pill:false},
-    {id:'5', medTitle:'Tolmetin',content:'for the shoulders',pill:true}
+    {id:'1', medTitle:'arveles',content:'for the headhack',pill:true, userId:'1'},
+    {id:'2',medTitle:'aferin',content:'for the stomache',pill:false, userId:'2'},
+    {id:'3',medTitle:'Ketorolak',content:'for the tooth',pill:false, userId:'3'},
+    {id:'4',medTitle:'Sulindak',content:'for the back',pill:false, userId:'4'},
+    {id:'5', medTitle:'Tolmetin',content:'for the shoulders',pill:true, userId:'5'}
+];
     
     
-]
+
 const medicationResolvers = {
     Query: {
       medications:() => medications
@@ -18,7 +19,7 @@ const medicationResolvers = {
   Mutation: {
    
       addMedication: (_,{medTitle, content, pill}) => {
-          const newMedication = {id:String(medications.length + 1),medTitle, content, pill};
+          const newMedication = {id:String(medications.length + 1),medTitle, content, pill, userId};
           medications.push(newMedication);
           return newMedication;
       },  
